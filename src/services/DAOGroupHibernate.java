e7fb982d68288fa048037f35ddadd711a7d96d12
Thomas Medioni <medioni.thomas@gmail.com>
2016-11-19 18:56:30 +0100

    integrate DAOGroup

--- src/services/DAOGroupHibernate.java
+++ src/services/DAOGroupHibernate.java
@@ -1,10 +1,8 @@
 package services;
 
-import org.hibernate.Session;
-import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 
 import domain.ContactGroup;
-import util.HibernateUtil;
 
 public class DAOGroupHibernate extends HibernateDaoSupport implements IDAOGroup {
 
@@ -16,18 +14,11 @@ public class DAOGroupHibernate extends HibernateDaoSupport implements IDAOGroup 
 		
 		group.setGroupName(name);
 		
-		//Obtention d’une session
-		Session session =
-		HibernateUtil.getSessionFactory().getCurrentSession();
-		//démarrer une transaction
-		session.beginTransaction();
-		//persister l’objet
-		session.save(group);
-		//recharger l’objet à partir de la session
-		group=(ContactGroup) session.load(ContactGroup.class, group.getGroupId());
-		//committer la transaction
-		session.getTransaction().commit();
 		
+		getHibernateTemplate().save(group);
+		//recharger l’objet à partir de la session
+		group=(ContactGroup) getHibernateTemplate().load(ContactGroup.class, group.getGroupId());
+
 		return group;
 	}
 
